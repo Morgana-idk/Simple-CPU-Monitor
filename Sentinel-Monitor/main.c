a77ef6f702e8ef3a7f109291b2b81353db71f15e
Morgana-idk <pedrokook123p@gmail.com>
2026-03-27 20:53:13 -0300

    adiciona suporte à biblioteca ncurses e implementa controle de opções no Sentinel Monitor

--- Sentinel-Monitor/main.c
+++ Sentinel-Monitor/main.c
@@ -2,20 +2,44 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
 int main() {
     
     int contagem = 0;
+    int opcao = 0;
+    int tecla;
+
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    curs_set(0);
 
     while (1){
-        printf("\033[H\033[J");
-        printf("\n\n==========SENTINEL MONITOR==========\n");
-        printf("%d\n", contagem);
-        printf("====================================\n\n");
+        erase();
+        mvprintw(0, 0, "========SENTINEL MONITOR========");
+        mvprintw(1, 0, "Contagem: %d", contagem);
+        mvprintw(3, 0, "Opcao: %d       (APERTE -> PARA MUDAR)", opcao);
+        mvprintw(4, 0, "Aperte Q para Sair.");
+        mvprintw(5, 0, "================================");
+
+        tecla = getch();
+
+        if (tecla == KEY_RIGHT){
+            opcao++;
+        } else if (tecla == KEY_LEFT) {
+            opcao--;
+        } else if (tecla == 'q' || tecla == 'Q') {
+            break;
+        }
+
+        refresh();
         contagem++;
-        sleep(1);
+        usleep(10000);
     }
     
-
+    endwin();
     return 0;
 }
